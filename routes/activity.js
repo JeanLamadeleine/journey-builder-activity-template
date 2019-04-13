3cd8af32e50ddecb86d3f095642f16230d41fc04
JeanLamadeleine <49563027+JeanLamadeleine@users.noreply.github.com>
2019-04-13 08:00:16 -0400

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -89,6 +89,7 @@ exports.execute = function (req, res) {
             
             logData(req);
             res.send(200, 'Execute');
+            //res.status(200).send('Execute')
         } else {
             console.error('inArguments invalid.');
             return res.status(400).end();
