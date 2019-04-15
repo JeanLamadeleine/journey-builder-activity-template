e1d33c41ac935b344824eef28c439d08faff61d0
JeanLamadeleine <49563027+JeanLamadeleine@users.noreply.github.com>
2019-04-15 11:18:37 -0400

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -86,7 +86,7 @@ exports.execute = function (req, res) {
             
             // decoded in arguments
             var decodedArgs = decoded.inArguments[0];
-            
+            console.error('in execute');
             logData(req);
             res.send(200, 'Execute');
             //res.status(200).send('Execute')
